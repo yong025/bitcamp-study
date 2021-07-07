94b6c4f96758d1f26cb3454ff063a4a35bec0463
yong025 <yong025@naver.com>
2021-07-07 17:47:55 +0900

    .

---
+++ camptest/app/src/main/java/com/yong/day0707/DBTest.java
@@ -0,0 +1,16 @@
+package com.yong.day0707;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBTest {
+
+  public static void main(String[] args) {
+    Connection CN=null; // DB서버 연결정보
+    Statement ST=null; // 명령어 생성
+    ResultSet RS =null; //조회 결과를 기억
+
+
+  }//main end
+}//class end
